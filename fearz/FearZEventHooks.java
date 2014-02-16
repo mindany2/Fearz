f97e59b4f448ee95251f5c1bbcbbc9a99979eb92
IcyFlurry <IcyFlurry@live.fr>
2014-02-16 14:59:24 +0100

    Update FearZEventHooks.java

--- fearz/FearZEventHooks.java
+++ fearz/FearZEventHooks.java
@@ -1,35 +1,40 @@
-package fearz;
+package FearZ;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 
 public class FearZEventHooks {
 
-	int a = 1;
-	@ForgeSubscribe
-	public void onEntityUpdate(LivingUpdateEvent event) {
-		if (event.entityLiving.isPotionActive(BaseFearZ.Fracture)) {
-			event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Fracture.id, 200000000,4));
-		}
-	}
-
-	@ForgeSubscribe
-	public void onLivingFall(LivingFallEvent event1)
-	{
-
-		if(event1.entityLiving instanceof EntityPlayer &&event1.distance > 6.0F )
-		{
-			EntityPlayer player = (EntityPlayer)event1.entityLiving;
-			if(a==1){
-				player.addChatMessage("Vous vous �tes cass� la jambe !"); a=0;}
-//			player.isSprinting();
-//			player.getMaxHealth()
-			event1.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Fracture.id, 200000000,1));
-
-		}
-
-	}     
+    int a = 1;
+    @ForgeSubscribe
+    public void onLivingFall(LivingFallEvent event1)
+    {
+   	 
+   			 if(event1.entityLiving instanceof EntityPlayer &&event1.distance > 6.0F &&event1.distance < 10.0F )
+         {
+           EntityPlayer player = (EntityPlayer)event1.entityLiving;
+           if(a==1){
+           player.addChatMessage("Vous vous êtes cassé la jambe !"); a=0;}
+           event1.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Fracture.id, 2000000000,0));
+           
+         }
+   			 
+   			if(event1.entityLiving instanceof EntityPlayer &&event1.distance > 10.0F )
+            {
+              EntityPlayer player = (EntityPlayer)event1.entityLiving;
+              if(a==1){
+              player.addChatMessage("Vous vous êtes cassé la jambe !"); a=0;}
+              event1.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.FractureOuverte.id, 2000000000,0));
+              
+            }
+   
+}     
 }
+
+
