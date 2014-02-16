8c63679b7787ce1395bac35a3805d26d0757bd23
IcyFlurry <IcyFlurry@live.fr>
2014-02-16 22:08:18 +0100

    Update ItemWater

--- fearz/BaseFearZ.java
+++ fearz/BaseFearZ.java
@@ -54,8 +54,8 @@ public class BaseFearZ {
 	public static Item Battery = (new Item(4011)).setUnlocalizedName("Battery").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:Battery");
 	public static Item Gourd = (new ItemGourd(4012)).setUnlocalizedName("Gourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:Gourd");
 	public static Item FilledGourd = (new Item(4013)).setUnlocalizedName("FilledGourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:FilledGourd");
-	public static Item PurifiedGourd = (new Item(4014)).setUnlocalizedName("PurifiedGourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedGourd");
-	public static Item PurifiedBottle = (new Item(4015)).setUnlocalizedName("PurifiedBottle").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedBottle");
+	public static Item PurifiedGourd = (new ItemWater(4014, 0, false)).setUnlocalizedName("PurifiedGourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedGourd");
+	public static Item PurifiedBottle = (new ItemWater(4015, 0, false)).setUnlocalizedName("PurifiedBottle").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedBottle");
 	public static Item Knife = (new ItemSword(4016, KnifeMat)).setUnlocalizedName("Knife").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:Knife");
 	public static Item StonyMass = (new ItemSword(4017, StonyMassMat)).setUnlocalizedName("StonyMass").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:StonyMass");
 	public static Item CrossBow = (new ItemCrossBow(4018)).setUnlocalizedName("CrossBow").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:CrossBow");
