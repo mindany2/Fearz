594e707bc43e3777c9ee01a8a5462e987ce8a558
IcyFlurry <IcyFlurry@live.fr>
2014-02-16 16:03:14 +0100

    Create MilkBucket.java

---
+++ fearz/MilkBucket.java
@@ -0,0 +1,26 @@
+package FearZ;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBucketMilk;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class MilkBucket extends ItemBucketMilk {
+
+	public MilkBucket(int par1) {
+		super(par1);
+
+	}
+
+	public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        if (!par3EntityPlayer.capabilities.isCreativeMode)
+        {
+            --par1ItemStack.stackSize;
+        }
+
+        return par1ItemStack.stackSize <= 0 ? new ItemStack(Item.bucketEmpty) : par1ItemStack;
+    }
+
+}
