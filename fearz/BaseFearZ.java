953e94a93beab90b2e07be854ac1e21f209b3574
mindany2 <robindesbois1000@hotmail.fr>
2014-02-17 20:51:36 +0100

    Update Degrès

--- fearz/BaseFearZ.java
+++ fearz/BaseFearZ.java
@@ -5,13 +5,17 @@ import java.lang.reflect.Modifier;
 
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.potion.Potion;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSourceIndirect;
+import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.Mod;
@@ -30,17 +34,18 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 public class BaseFearZ {
 	
 	@Instance("FearZ")
-	public static DamageSource causeArrowDamage(EntityTilesCrossbow par0EntityArrow, Entity par1Entity)
-    {
-        return (new EntityDamageSourceIndirect("tilesCrossbow", par0EntityArrow, par1Entity)).setProjectile();
-    }
 	public static BaseFearZ instance;
 	public static Potion Fracture;
 	public static Potion FractureOuverte;
 	public static Potion Maladie;
+	public static Potion Degres;
 	public static CreativeTabs FearzCreativeTabs = new FearzCreativeTabs(CreativeTabs.getNextID(), "FearZ");
 	static EnumToolMaterial KnifeMat = net.minecraftforge.common.EnumHelper.addToolMaterial("KnifeMat", 2, 1561, 6.0F, 2.0F, 14);
 	static EnumToolMaterial StonyMassMat = net.minecraftforge.common.EnumHelper.addToolMaterial("StonyMassMat", 2, 131, 6.0F, 1.0F, 14);
+	public static DamageSource causeArrowDamage(EntityTilesCrossbow par0EntityArrow, Entity par1Entity)
+    {
+        return (new EntityDamageSourceIndirect("tilesCrossbow", par0EntityArrow, par1Entity)).setProjectile();
+    }
 	
 	
 	//items
@@ -54,14 +59,14 @@ public class BaseFearZ {
 	public static Item Battery = (new Item(4011)).setUnlocalizedName("Battery").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:Battery");
 	public static Item Gourd = (new ItemGourd(4012)).setUnlocalizedName("Gourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:Gourd");
 	public static Item FilledGourd = (new Item(4013)).setUnlocalizedName("FilledGourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:FilledGourd");
-	public static Item PurifiedGourd = (new ItemWater(4014, 0, false)).setUnlocalizedName("PurifiedGourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedGourd");
-	public static Item PurifiedBottle = (new ItemWater(4015, 0, false)).setUnlocalizedName("PurifiedBottle").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedBottle");
+	public static Item PurifiedGourd = (new Item(4014)).setUnlocalizedName("PurifiedGourd").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedGourd");
+	public static Item PurifiedBottle = (new Item(4015)).setUnlocalizedName("PurifiedBottle").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:PurifiedBottle");
 	public static Item Knife = (new ItemSword(4016, KnifeMat)).setUnlocalizedName("Knife").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:Knife");
 	public static Item StonyMass = (new ItemSword(4017, StonyMassMat)).setUnlocalizedName("StonyMass").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:StonyMass");
 	public static Item CrossBow = (new ItemCrossBow(4018)).setUnlocalizedName("CrossBow").setCreativeTab(BaseFearZ.FearzCreativeTabs).setTextureName("fearz:CrossBow");
 	public static Item MilkBucket;
 	
-	
+
 	@EventHandler 
     public void initConfig(FMLPreInitializationEvent event) 
     { 
@@ -102,8 +107,9 @@ public class BaseFearZ {
     	   Item.itemsList[Item.bucketMilk.itemID] = MilkBucket;
     	Fracture = (new Fracture(32, false, 0)).setIconIndex(0, 0).setPotionName("Fracture").func_111184_a(SharedMonsterAttributes.movementSpeed, "7107DE5E-7CE8-4030-940E-514C1F160890", -0.45000000596046448D, 2);
     	FractureOuverte = (new Fracture(33, false, 0)).setIconIndex(0, 0).setPotionName("Fracture Ouverte").func_111184_a(SharedMonsterAttributes.movementSpeed, "7107DE5E-7CE8-4030-940E-514C1F160890", -0.45000000596046448D, 2).func_111184_a(SharedMonsterAttributes.maxHealth, "5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", -6.0D, 0);
-    	Maladie = (new Maladie(34, true, 0)).setIconIndex(0, 0).setPotionName("Maladie").func_111184_a(SharedMonsterAttributes.attackDamage, "22653B89-116E-49DC-9B6B-9971489B5BE5", 5.00D, 0);
-         
+     	Maladie = (new Maladie(34, true, 0)).setIconIndex(0, 0).setPotionName("Maladie").func_111184_a(SharedMonsterAttributes.attackDamage, "22653B89-116E-49DC-9B6B-9971489B5BE5", 5.00D, 0);
+     	Degres = (new Fracture(35, true, 0)).setIconIndex(0, 0).setPotionName("Degrès");
+             
     }
     
     
--- fearz/FearZEventHooks.java
+++ fearz/FearZEventHooks.java
@@ -6,53 +6,91 @@ import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionAttackDamage;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 
 public class FearZEventHooks {
 
+	public int deg=37; 
+//	BaseFearZ FearZ = new BaseFearZ();
 	@ForgeSubscribe
 	public void onEntityUpdate(LivingUpdateEvent event) {
+		int i = MathHelper.floor_double(event.entity.posX);
+		int j = MathHelper.floor_double(event.entity.posZ);
+		int b = 0;
+		if(event.entityLiving instanceof EntityPlayer){
+			EntityPlayer player = (EntityPlayer)event.entityLiving;
+		
 		if (event.entityLiving.isPotionActive(BaseFearZ.Fracture)) {
 			event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Fracture.id, 200000000,0));
 		}
 		if (event.entityLiving.isPotionActive(BaseFearZ.FractureOuverte)) {
 			event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.FractureOuverte.id, 200000000,0));
-			
+
 		}
 		if (event.entityLiving.isPotionActive(BaseFearZ.Maladie)) {
 			if (event.entityLiving.getActivePotionEffect(BaseFearZ.Maladie).getDuration()==1){
 				event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Maladie.id, 6000,0));
 				event.entityLiving.attackEntityFrom(DamageSource.magic, 1.0F);
-					}}
-			
+			}}
+		if(event.entity.worldObj.getBiomeGenForCoords(i, j) == BiomeGenBase.swampland){
+			if (!event.entityLiving.isPotionActive(BaseFearZ.Degres)) {
+			event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Degres.id, 500,0));
 		}
-	
-    int a = 1;
-    @ForgeSubscribe
-    public void onLivingFall(LivingFallEvent event1)
-    {
-   	 
-   			 if(event1.entityLiving instanceof EntityPlayer &&event1.distance > 6.0F &&event1.distance < 10.0F )
-         {
-           EntityPlayer player = (EntityPlayer)event1.entityLiving;
-           if(a==1){
-           player.addChatMessage("Vous vous êtes cassé la jambe !"); a=0;}
-           event1.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Fracture.id, 2000000000,0));
-           
-         }
-   			 
-   			if(event1.entityLiving instanceof EntityPlayer &&event1.distance > 10.0F )
-            {
-              EntityPlayer player = (EntityPlayer)event1.entityLiving;
-              if(a==1){
-              player.addChatMessage("Vous vous êtes cassé la jambe !"); a=0;}
-              event1.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.FractureOuverte.id, 2000000000,0));
-              
-            }
-   
-}     
+		}
+			if (event.entityLiving.isPotionActive(BaseFearZ.Degres)) {
+				if (event.entityLiving.getActivePotionEffect(BaseFearZ.Degres).getDuration()==20){
+				b = 0;
+				}
+				if (event.entityLiving.getActivePotionEffect(BaseFearZ.Degres).getDuration()==2&& b==0){
+					if(b==0){
+							deg--;
+					b=5;
+					player.addChatMessage(Integer.toString(b));
+
+					player.addChatMessage("Votre temp�rature corporelle est de "+deg+"�C");
+//					event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Degres.id, 2000,0));
+				}
+				}
+		}
+
+			if(deg == 35){
+				if (event.entityLiving.isPotionActive(BaseFearZ.Maladie)) {
+				event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Maladie.id, 6000,0));
+			}
+			}
+		//
+		//			event.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.FractureOuverte.id, 200000000,0));
+		//		}
+	}}
+
+	int a = 1;
+	@ForgeSubscribe
+	public void onLivingFall(LivingFallEvent event1)
+	{
+
+		if(event1.entityLiving instanceof EntityPlayer &&event1.distance > 6.0F &&event1.distance < 10.0F )
+		{
+			EntityPlayer player = (EntityPlayer)event1.entityLiving;
+			if(a==1){
+				player.addChatMessage("Vous vous êtes cassé la jambe !"); a=0;}
+			event1.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.Fracture.id, 2000000000,0));
+
+		}
+
+		if(event1.entityLiving instanceof EntityPlayer &&event1.distance > 10.0F )
+		{
+			EntityPlayer player = (EntityPlayer)event1.entityLiving;
+			if(a==1){
+				player.addChatMessage("Vous vous êtes cassé la jambe !"); a=0;}
+			event1.entityLiving.addPotionEffect(new PotionEffect(BaseFearZ.FractureOuverte.id, 2000000000,0));
+
+		}
+
+	}     
 }
 
 
